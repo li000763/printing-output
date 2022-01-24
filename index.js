dd35d13f1698ac61b9e28a0b3bdc98c7117d0223
li000763 <98288371+li000763@users.noreply.github.com>
2022-01-24 01:53:06 -0500

    javascript file added

---
+++ index.js
@@ -0,0 +1,41 @@
+let a = 3;
+let b = 5;
+let c;
+
+// alert( "description of alerted_value: " + alerted_value );
+/*
+a + b
+a - b
+a * b
+a / b
+a % b
+a += b
+a -= b
+a *= b
+a /= b
+a %= b
+a == b
+a != b
+a > b
+a < b
+!a && !c 
+!a || !c 
+*/
+
+
+alert(a + b); // 8
+alert(a - b); // -2
+alert(a * b); // 15
+alert(a / b); // 0.6
+alert(a % b); // 3
+alert(a += b); // 8
+alert(a -= b); // 3
+alert(a *= b); // 15
+alert(a /= b); // 3
+alert(a %= b); // 3
+alert(a == b); // false
+alert(a != b); // true
+alert(a > b); // false
+alert(a < b); // true
+alert(!a && !c); // false
+alert(!a || !c ); // true
